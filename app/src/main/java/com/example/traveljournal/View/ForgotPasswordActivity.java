2f11c3d4ef1e0c1ecec6b7de115d87bcfec4e6f7
Vasilachi Eugen <vasilachi38@yahoo.com>
2022-04-27 02:59:56 +0300

    forgot password and create an account ui

--- app/src/main/java/com/example/traveljournal/View/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/traveljournal/View/ForgotPasswordActivity.java
@@ -3,32 +3,36 @@ package com.example.traveljournal.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.traveljournal.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
-    private EditText editTextEmail, editTextCode;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        editTextEmail = findViewById(R.id.verifyEmail);
-        editTextCode = findViewById(R.id.verifyCode);
+        email = findViewById(R.id.emailAddress);
+
+        getSupportActionBar().hide();
     }
 
-    public void searchEmail(View view) {
-        String email = editTextEmail.getText().toString();
-        LoginActivity.checkEmail(email, editTextEmail);
-    }
+    public void sendEmail(View view) {
+        String emailAddress = email.getText().toString();
 
-    public void verifyCode(View view) {
-        String code = editTextCode.getText().toString();
-        if(code.isEmpty()){
-            editTextCode.setError("Please add a code");
+        if (emailAddress.isEmpty()) {
+            email.setError("Please add an email address");
+
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+            email.setError("Wrong format of email address");
+        } else {
+            Toast.makeText(this,"Email send to Register Email Address",Toast.LENGTH_LONG).show();
         }
     }
 }
