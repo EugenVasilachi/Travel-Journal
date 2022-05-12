f4d9bf8960f73c81f5df9fd0da49502e04e71b40
Vasilachi Eugen <vasilachi38@yahoo.com>
2022-05-12 18:21:28 +0300

    lala

--- app/src/main/java/com/example/traveljournal/View/SplashActivity.java
+++ app/src/main/java/com/example/traveljournal/View/SplashActivity.java
@@ -10,7 +10,7 @@ import com.example.traveljournal.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1500;
+    private static final int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
